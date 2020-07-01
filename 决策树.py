4a8b85b13614f93e23dfccc42494424b1db81a89
troywu666 <kpwcy@qq.com>
2020-07-01 10:23:30 +0800

    update 决策树.py

--- 决策树.py
+++ 决策树.py
@@ -4,7 +4,7 @@
 @Autor: Troy Wu
 @Date: 2020-06-30 09:12:32
 @LastEditors: Troy Wu
-@LastEditTime: 2020-06-30 21:58:26
+@LastEditTime: 2020-07-01 10:23:09
 '''
 import numpy as np
 
@@ -103,3 +103,124 @@ class DecisionTree:
         if hasattr(self, 'tree_'):
             return str(self.tree_)
         return ''
+
+class CartClassificationTree:
+    class Node:
+        def __init__(self):
+            self.value = None
+            self.feature_index = None
+            self.feature_value = None
+            self.left = None
+            self.right = None
+
+        def __str__(self):
+            if self.left:
+                s = '内部节点<{}>\n'.format(self.feature_index)
+                ss = '[>{}]->{}'.format(self.feature_index, self.left)
+                s += '\t' + ss.replace('\n', '\n\t') + '\n'
+                ss = '[<={}]->{}'.format(self.feature_value, self.right)
+                s += '\t' + ss.replace('\n', '\n\t')
+            else:
+                s = '叶节点({})'.format(self.value)
+            return s
+
+    def __init__(self, gini_threshold = 0.01, gini_dec_threshold = 0., min_samples_split = 2):
+        # 基尼系数降低的阈值
+        self.gini_dec_threshold = gini_dec_threshold
+        # 基尼系数的阈值
+        self.gini_threshold = gini_threshold
+        # 数据集还可继续切分的最小样本数量
+        self.min_samples_split = min_samples_split
+    
+    def _gini(self, y):
+        values = np.unique(y)
+        s = 0.
+        for v in values:
+            y_sub = y[y == v]
+            s += (y_sub.size / y.size) ** 2
+        return 1 - s
+
+    def _gini_split(self, y, feature, value):
+        '''计算根据特征切分后的基尼系数'''
+        indices = feature > value
+        y1 = y[indices]
+        y2 = y[~indices]
+        gini1 = self._gini(y1)
+        gini2 = self._gini(y2)
+        gini = (y1.size * gini1 + y2.size * gini2) / y.size
+        return gini
+
+    def _get_split_points(self, feature):
+        '''获得一个连续特征的所有切分点'''
+        values = np.unique(feature)
+        split_points = [(v1+v2)/2 for v1, v2, in zip(values[: -1], values[1:])]
+        return split_points
+
+    def _select_feature(self, X, y):
+        '''选择划分特征'''
+        best_feature_index = None
+        best_split_value = None
+        min_gini = np.inf
+        _, n = X.shape
+        for feature_index in range(n):
+            feature = X[:, feature_index]
+            split_points = self._get_split_points(feature)
+            for value in split_points:
+                # 迭代每一个分割点value，计算使用value分割后的数据集基尼系数
+                gini = self._gini_split(y, feature, value)
+                if gini < min_gini:
+                    min_gini = gini
+                    best_feature_index = feature_index
+                    best_split_value = value
+
+        if self._gini(y) - min_gini < self.gini_dec_threshold:
+            best_feature_index = None
+            best_split_value = None
+        return best_feature_index, best_split_value, min_gini
+
+    def _node_value(self, y):
+        '''计算节点的值'''
+        label_counts = np.bincount(y)
+        return np.argmax(label_counts)
+
+    def _create_tree(self, X, y):
+        '''生成树递归算法'''
+        # 创建节点
+        node = self.Node()
+        # 计算节点的值
+        node.value = self._node_value(y)
+        # 若当前数据集样本数量小于最小分割数量min_samples_split，则返回叶节点
+        if y.size < self.min_samples_split:
+            return node
+        # 若当前数据集的基尼系数小于阈值gini_threshold，则返回叶节点
+        if self._gini(y) < self.gini_threshold:
+            return node
+
+        # 选择最佳分割特征
+        feature_index, feature_value, min_gini = self._select_feature(X, y)
+        if feature_index is not None:
+            node.feature_index = feature_index
+            node.feature_value = feature_value
+            feature = X[:, feature_index]
+            indices = feature > feature_value
+            X1, y1 = X[indices], y[indices]
+            X2, y2 = X[~indices], y[~indices]
+            node.left = self._create_tree(X1, y1)
+            node.right = self._create_tree(X2, y2)
+
+        return node
+
+    def _predict_one(self, X_test):
+        node = self.tree_
+        while node.left:
+            if X_test[node.feature_index] < node.feature_value:
+                node = node.left
+            else:
+                node = node.right
+        return node.value
+
+    def train(self, X_train, y_train):
+        self.tree_ = self._create_tree(X_train, y_train)
+
+    def predict(self, X_test):
+        return np.apply_along_axis(self._predict_one, axis = 1, arr = X_test)
